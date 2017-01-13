e1efa6fe0dba76f08bc2906f8cd0153ba4684aae
junliang <aaa@163.com>
2017-01-13 15:25:30 +0800

    该引擎后面主要是在做ui方面的研究。 最新的日志上看，貌似有体素？目前没兴趣； 特效？没有资源，无法运行。 所以更新到比较原始的一个版本，对我帮助挺大。

--- CubeEngine/Application/CubeGame/GameConfig.cpp
+++
@@ -1,7 +0,0 @@
-#include "GameConfig.h"
-float BLOCK_SIZE = 0.5f;
-int MAX_BLOCK = 16;
-int GAME_MAP_WIDTH = 48;
-int GAME_MAP_DEPTH = 48;
-int GAME_MAP_HEIGHT = 4;
-int CUBE_MAP_SIZE = 128;
--- CubeEngine/EngineSrc/Engine/EngineDef.cpp
+++
@@ -1,9 +0,0 @@
-#include "EngineDef.h"
-
-namespace tzw {
-int EngineDef::maxPiority = 999;
-int EngineDef::debugPanelPiority = 100;
-const char * EngineDef::versionStr = "Cube Engine Alpha v0.22";
-
-int EngineDef::focusPiority = 997;
-} // namespace tzw
